15c2b698fcee5cf8f40c36b0f68431e705dc37ba
Milind241 <milinddadore123@gmail.com>
2024-04-04 21:07:16 +0530

    First commit

---
+++ form/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Signup from './Signup'
+import './App.css'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <div>
+        <Signup/>
+      </div>
+  )
+}
+
+export default App
